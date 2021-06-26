7de48ca036b63d78d8ea736cea577db9101d554d
JulianaMoreno <julianavieiramoreno@gmail.com>
2021-06-25 23:09:31 -0300

    add room page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,8 +1,43 @@
- export const Room = () => {
-     return (
-        <h1>Bem vindo!</h1>
-     )
- }
+import { useParams } from 'react-router-dom';
+import Button from '../components/Button';
+import RoomCode from '../components/RoomCode';
+import '../styles/room.scss';
 
+export const Room = () => {
+    const params = useParams<RoomParams>();
+
+     return (
+         <div id="page-room">
+             <header>
+                 <div className="content">
+                     <strong>Quest.io</strong>
+                     <RoomCode code={params.id}/>
+                 </div>
+             </header>
+
+             <main>
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea
+                    placeholder="Faça sua pergunta aqui"/>
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta,
+                            <button>faça seu login</button>
+                        </span>
+                        <Button type="submit">Enviar Pergunta</Button>
+                    </div>
+                </form>
+             </main>
+         </div>
+     )
+}
+
+type RoomParams = {
+    id: string;
+}
 
  export default Room;
